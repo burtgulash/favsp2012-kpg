e07f58fe746ad7de3faafc5edcce7a03644cb2d3
gusta <burtgulas@gmail.com>
2012-02-18 15:22:34 +0100

    maze

---
+++ maze/Maze.java
@@ -0,0 +1,118 @@
+import java.util.Stack;
+import java.util.Random;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class Maze {
+	static boolean[][] visited;
+	static boolean[][][] wall;
+	static final Random gorila = new Random();
+	static final int[][] permutation = {
+				{1, 2, 3, 4}, {1, 2, 4, 3}, {1, 3, 2, 4}, {1, 3, 4, 2},
+				{1, 4, 2, 3}, {1, 4, 3, 2}, {2, 1, 3, 4}, {2, 1, 4, 3},
+				{2, 3, 1, 4}, {2, 3, 4, 1}, {2, 4, 1, 3}, {2, 4, 3, 1},
+				{3, 1, 2, 4}, {3, 1, 4, 2}, {3, 2, 1, 4}, {3, 2, 4, 1},
+				{3, 4, 1, 2}, {3, 4, 2, 1}, {4, 1, 2, 3}, {4, 1, 3, 2},
+				{4, 2, 1, 3}, {4, 2, 3, 1}, {4, 3, 1, 2}, {4, 3, 2, 1}};
+
+	static void generate(int start, int rows, int cols) {
+		visited = new boolean[rows][cols];
+		wall    = new boolean[rows][cols][2];
+
+		Stack<Integer> s = new Stack<Integer>();
+		s.push(start);
+
+		while (!s.empty()) {
+			int pos = s.pop();
+			int r = pos / cols;
+			int c = pos % cols;
+
+			int rand = Math.abs(gorila.nextInt()) % 24;
+			for (int i = 1; i <= 4; i++) {
+				switch(permutation[rand][i - 1]) {
+				case 1: 
+					if(c + 1 < cols && !visited[r][c + 1]) {
+						visited[r][c + 1] = true;
+						wall[r][c][0] = true;
+						s.push(r * cols + c + 1);
+					}
+					break;
+				case 2:
+					if(r > 0 && !visited[r - 1][c]) {
+						visited[r - 1][c] = true;
+						wall[r - 1][c][1] = true;
+						s.push(r * cols - cols + c);
+					}
+					break;
+				case 3:
+					if(c > 0 && !visited[r][c - 1]) {
+						visited[r][c - 1] = true;
+						wall[r][c - 1][0] = true;
+						s.push(r * cols + c - 1);
+					}
+					break;
+				case 4:
+					if(r + 1 < rows && !visited[r + 1][c]) {
+						visited[r + 1][c] = true;
+						wall[r][c][1] = true;
+						s.push(r * cols + cols + c);
+					}
+					break;
+				}
+			}
+		}
+
+	}
+
+
+	public static void main (String[] args) {
+		int rows = 400;
+		int cols = 400;
+
+		int width = 800;
+		int height = width * rows / cols;
+
+		int boxSize = width / rows;
+		int wallThick = boxSize / 2;
+		int innerBoxSize = boxSize - wallThick;
+		
+
+		BufferedImage img = new BufferedImage(width + wallThick, 
+                                              height + wallThick, 
+                                              BufferedImage.TYPE_INT_RGB);
+		Graphics2D gg = img.createGraphics();
+		
+		generate(0, rows, cols);
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				gg.fillRect(j * boxSize + wallThick, 
+                            i * boxSize + wallThick, 
+                            innerBoxSize, innerBoxSize);
+
+				if (wall[i][j][0])
+					gg.fillRect((j + 1) * boxSize, 
+                                 i * boxSize + wallThick, 
+                                 wallThick, innerBoxSize);
+				if (wall[i][j][1])
+					gg.fillRect(j * boxSize + wallThick,
+                                (i + 1) * boxSize, 
+                                innerBoxSize, wallThick);
+			}
+		}
+
+		gg.fillRect(wallThick, 0, innerBoxSize, wallThick);
+		gg.fillRect((cols - 1) * boxSize + wallThick, 
+                    rows * boxSize, innerBoxSize, wallThick);
+
+		
+		try {
+			ImageIO.write(img, "png", new File("maze.png"));
+		} catch (IOException ioe) {
+		}
+	}
+}
